1a2754b6808c9c62820554124aeb2127f4e41647
quadiez <dduidokdoi@gmail.com>
2023-03-19 23:01:18 +0700

    fix: offset

--- src/components/landing/contacts/ContactSectionParallax.tsx
+++ src/components/landing/contacts/ContactSectionParallax.tsx
@@ -18,7 +18,7 @@ export const ContactSectionParallax = () => {
         />
       </ParallaxLayer>
       <ParallaxLayer
-        offset={2}
+        offset={1}
         speed={-0}
         style={{ width: "fit-content", height: "fit-content" }}
       >
